da2823c92f42f7ed7a9fdcfbbbe098ccdd554b75
Noble Benjamin <nobleben2008@gmail.com>
2024-06-18 22:28:55 +0100

    Started app layout by adding empty Logo component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Logo from './components/Logo'
+
+const App = () => {
+  return (
+    <>
+        <Logo/>
+    </>
+  )
+}
+
+export default App
