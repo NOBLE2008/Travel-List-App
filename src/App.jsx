55cc3703ba33fd68b386c28ff7303fe587590d7b
Noble Benjamin <nobleben2008@gmail.com>
2024-06-18 22:30:20 +0100

    Added empty Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Logo from './components/Logo'
+import Form from './components/Form'
 
 const App = () => {
   return (
     <>
         <Logo/>
+        <Form/>
     </>
   )
 }
