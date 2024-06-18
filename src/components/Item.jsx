ae4eff3e100873e1b5bd3d13561f8524a11458c0
Noble Benjamin <nobleben2008@gmail.com>
2024-06-18 22:25:08 +0100

    Added Hard Coded List Items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 const Item = () => {
   return (
     <div>
