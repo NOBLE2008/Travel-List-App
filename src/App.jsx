4cb1a6f445b1f776a764c7a9acb252bde5ec2843
Noble Benjamin <nobleben2008@gmail.com>
2024-06-18 22:31:32 +0100

    Added empty Packaging List component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Logo from './components/Logo'
 import Form from './components/Form'
+import PackagingList from './components/PackagingList'
 
 const App = () => {
   return (
     <>
         <Logo/>
         <Form/>
+        <PackagingList/>
     </>
   )
 }
