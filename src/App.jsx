3ab4b3a1855503aa0254e83cbadfe45b1807422e
Noble Benjamin <nobleben2008@gmail.com>
2024-06-19 23:33:44 +0100

    Removed travelList array from list comp

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Form from './components/Form'
 import PackagingList from './components/PackagingList'
 import Stats from './components/Stats'
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 const App = () => {
   return (
     <div className='app'>
