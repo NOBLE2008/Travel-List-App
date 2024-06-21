35598bc67e4c13e450db218c2fa878ce6c3731f8
Noble Benjamin <nobleben2008@gmail.com>
2024-06-21 21:57:07 +0100

    Changed Input to select in the form comp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,9 @@ const Form = () => {
   return (
     <form className="add-form">
       <h3>What do you need for your trip?</h3>
-      <input type="number"/>
+      <select>
+        
+      </select>
       <button type="submit">Add</button>
     </form>
   );
