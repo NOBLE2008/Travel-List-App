e1cf3fe5e695e0cffbabc019f069e558cbe5d6b9
Noble Benjamin <nobleben2008@gmail.com>
2024-06-21 21:38:18 +0100

    Added form element to the Form Component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,7 @@
 const Form = () => {
   return (
-    <div className="add-form">
-      <h3>What do you need for your trip?</h3>
-    </div>
+    <form className="add-form">
+    </form>
   );
 };
 
