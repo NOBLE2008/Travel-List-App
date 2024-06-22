dfaccb47407767f4ab1536a1da74d689eccc6a78
Noble Benjamin <nobleben2008@gmail.com>
2024-06-22 21:36:38 +0100

    Fully initialized items

--- src/components/PackagingList.jsx
+++ src/components/PackagingList.jsx
@@ -1,13 +1,7 @@
 import { useState } from "react";
 
 const PackagingList = () => {
-  const [initialItems, setInitial] = useState([
-    { id: 'bznonx45', description: "Passports", quantity: 2, packed: false },
-    { id: 'hssh84', description: "Socks", quantity: 12, packed: false },
-    { id: 'ainozs849s', description: "Shoes", quantity: 1, packed: false },
-    { id: 'beang585na', description: "Shirts", quantity: 1, packed: false },
-    { id: 'fngoi9syh', description: "Pants", quantity: 1, packed: false },
-  ]);
+  const [initialItems, setInitial] = useState([]);
   
 
   const del = (num) => {
