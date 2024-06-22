b60e4f7b67b55500c9a1b0ae7a5068849cb88dfc
Noble Benjamin <nobleben2008@gmail.com>
2024-06-22 21:33:23 +0100

    Added Delete Items functionality

--- src/components/PackagingList.jsx
+++ src/components/PackagingList.jsx
@@ -2,19 +2,35 @@ import { useState } from "react";
 
 const PackagingList = () => {
   const [initialItems, setInitial] = useState([
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-    { id: 3, description: "Shoes", quantity: 1, packed: false },
-    { id: 4, description: "Shirts", quantity: 1, packed: false },
-    { id: 5, description: "Pants", quantity: 1, packed: false },
+    { id: 'bznonx45', description: "Passports", quantity: 2, packed: false },
+    { id: 'hssh84', description: "Socks", quantity: 12, packed: false },
+    { id: 'ainozs849s', description: "Shoes", quantity: 1, packed: false },
+    { id: 'beang585na', description: "Shirts", quantity: 1, packed: false },
+    { id: 'fngoi9syh', description: "Pants", quantity: 1, packed: false },
   ]);
-  const render = (item, i) => {
-    <li>
-    <span>
-    <input type="checkbox" />{item.description} <pre>❌</pre>
-  </span>
-    </li>
+  
+
+  const del = (num) => {
+    return () => {
+      return setInitial((item) => {
+        return item.filter((ite) => ite.id!== num);
+      });
+    }
   };
+  const render = (item, num) => (
+    <li>
+      <span>
+        <input type="checkbox" />
+        {item.description}{" "}
+        <pre
+          onClick={del(item.id)}
+        >
+          ❌
+        </pre>
+      </span>
+    </li>
+  );
+
   return (
     <div className="list">
       <ul>{initialItems.map(render)}</ul>
