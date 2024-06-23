ea7c158b7553be46f0d0eafd01a522e6e6692171
Noble Benjamin <nobleben2008@gmail.com>
2024-06-23 17:43:11 +0100

    Changed cursor of the delete X button

--- src/components/PackagingList.jsx
+++ src/components/PackagingList.jsx
@@ -12,7 +12,7 @@ const PackagingList = (props) => {
       <span>
         <input type="checkbox" />
         {item.description} {item.quantity}
-        <pre onClick={del(item.id)}>❌</pre>
+        <pre onClick={del(item.id)} className="del">❌</pre>
       </span>
     </li>
   );
