394a094640c588645dcf3affcded1697c2a577aa
Noble Benjamin <nobleben2008@gmail.com>
2024-06-29 19:11:20 +0100

    Fixed a bug for generation of stat messages

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,15 @@
 const Stats = ({ items }) => {
+  if (items.length === 0) {
+    return <footer>
+    <em>Start adding some items to your packing list 💼</em>
+  </footer>
+  }
   const numItems = items.length;
   const numPacked = items.filter(item => item.packed).length;
   const packedPercentage = numPacked===0 ? 0 : Math.round((numPacked/numItems)*100)
   return (
     <footer>
-      <em>{packedPercentage===100? `You got Everything. Ready to go✈️`: packedPercentage===0 ? 'Start adding some items to your Packaging list 💼' : `💼 You have ${numItems} items on your list and you already packed ${numPacked} (${packedPercentage}%)`}</em>
+      <em>{packedPercentage===100 ? 'You have everything! Ready to go ✈️': `You have ${numItems} in your list and you already packed ${numPacked} (${packedPercentage}%)`}</em>
     </footer>
   );
 };
