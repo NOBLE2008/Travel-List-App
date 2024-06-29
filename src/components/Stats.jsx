0bb6ca9b6a129b7ac24af626e65f710b8600f7dc
Noble Benjamin <nobleben2008@gmail.com>
2024-06-29 16:50:38 +0100

    Added a variable for number of items Packed

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,6 @@
 const Stats = ({ items }) => {
   const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
   return (
     <footer>
       <em>💼 You have {numItems} items on your list and you already packed X (X%)</em>
