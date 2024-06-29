32832f4ab7f52f55eac9495b25fd304e8f05e182
Noble Benjamin <nobleben2008@gmail.com>
2024-06-29 18:39:45 +0100

    Added a Packed Percentage Variable

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,7 @@
 const Stats = ({ items }) => {
   const numItems = items.length;
   const numPacked = items.filter(item => item.packed).length;
+  const packedPercentage = Math.round((numPacked/numItems)*100)
   return (
     <footer>
       <em>💼 You have {numItems} items on your list and you already packed {numPacked} (X%)</em>
