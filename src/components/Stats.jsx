e3efed5d379d7dd2ed1b6db4d211b727772d019e
Noble Benjamin <nobleben2008@gmail.com>
2024-06-29 16:47:20 +0100

    Display numItems in the UI

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,7 +2,7 @@ const Stats = ({ items }) => {
   const numItems = items.length;
   return (
     <footer>
-      <em>💼 You have X items on your list and you already packed X (X%)</em>
+      <em>💼 You have {numItems} items on your list and you already packed X (X%)</em>
     </footer>
   );
 };
