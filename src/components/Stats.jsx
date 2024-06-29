2f65ae34c32ef0a8040e6c5fae56f8c0806fc57f
Noble Benjamin <nobleben2008@gmail.com>
2024-06-29 18:58:10 +0100

    Added a message for when there is no list

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,7 +4,7 @@ const Stats = ({ items }) => {
   const packedPercentage = numPacked===0 ? 0 : Math.round((numPacked/numItems)*100)
   return (
     <footer>
-      <em>{packedPercentage===100? `You got Everything. Ready to go✈️`:  `💼 You have ${numItems} items on your list and you already packed ${numPacked} (${packedPercentage}%)`}</em>
+      <em>{packedPercentage===100? `You got Everything. Ready to go✈️`: packedPercentage===0 ? 'Start adding some items to your Packaging list 💼' : `💼 You have ${numItems} items on your list and you already packed ${numPacked} (${packedPercentage}%)`}</em>
     </footer>
   );
 };
